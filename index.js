fccf1b961c835f598c8f92d56191588743d48e5a
Lesandrod <lentrix12345@gmail.com>
2023-03-27 18:29:58 -0500

    conversion a express- lennard

---
+++ index.js
@@ -0,0 +1,31 @@
+    const express = require('express')
+    const app =  express()
+    
+    app.get('/',(req,res) =>{
+        res.sendFile('./static/index.html',{
+            root:__dirname});
+    });
+    app.get('/contacto',(req,res) =>{
+        res.sendFile('./static/contacto.html',
+        {root:__dirname});
+    });
+    app.get('/nosotros',(req,res) =>{
+        res.sendFile('./static/nosotros.html',{
+            root:__dirname});
+    });
+    app.get('/clientes',(req,res) =>{
+      res.sendFile('./static/clientes.html',{
+          root:__dirname});
+    });
+    app.get('/servicios',(req,res) =>{
+      res.sendFile('./static/servicio.html',{
+          root:__dirname});
+    });
+    app.use((req,res) => {
+        res.status(404).sendFile('./static/404.html',{
+            root:__dirname});
+    });
+    
+    
+    app.listen(3000);
+    console.log(`Server on port ${3000}`);
